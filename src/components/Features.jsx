6381de89d95201fee4d42d1375d1693357cc40db
Rachit Narula <narularachit22@gmail.com>
2023-10-23 02:38:15 +0530

    responsive update

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -39,18 +39,19 @@ const data = [
 ];
 
 const Features = () => {
-    return (
-      <div className='grid grid-cols-3 gap-4 justify-center mx-36 my-40' id='features'>
-        {data.map((item, index) => (
-          <div key={index} className="bg-white p-6 flex flex-col items-center">
-            <img src={item.img} alt="" />
-            <h1 className="text-xl font-bold mb-2">{item.title}</h1>
-            <p className="text-sm text-gray-600">{item.desc}</p>
-          </div>
-        ))}
-      </div>
-    );
-  };
+  return (
+    <div className='grid grid-cols-3  sml:grid-cols-1 med:grid-cols-3 gap-4 justify-center mx-4 sml::mx-8 med::mx-16 my-28 ' id='features'>
+      {data.map((item, index) => (
+        <div key={index} className="bg-white p-6 flex flex-col items-center">
+          <img src={item.img} alt="" className="max-w-full h-auto mb-4" />
+          <h1 className="text-lg sm:text-xl font-bold mb-2">{item.title}</h1>
+          <p className="text-sm text-gray-600 text-center">{item.desc}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
   
 
 export default Features;
